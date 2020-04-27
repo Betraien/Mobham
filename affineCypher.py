aac13e233df4f7815898da89e8c36406cb8eb1b9
Shabib <48416467+uhBetrayal@users.noreply.github.com>
2020-04-27 05:36:27 +0300

    Update affineCypher.py

--- affineCypher.py
+++ affineCypher.py
@@ -37,7 +37,6 @@ def handleInput(text):
     return inputArray
         
 def convertInputToArabicPosition(input):
-    print (input)
     inputToNumber = []
     for i in input:
         if i == " ":
@@ -54,7 +53,6 @@ def cypher(input, key, key2):
     encryptedTextArray= []
     for i in range(len(processed_input)):
         if processed_input[i] != -1:
-            #the line that handles the encryption of every charecter
              processed_input[i] = ((processed_input[i] * keysA[key2%len(keysA)])+(keyB))%(28)
     for x in processed_input:
         if x != -1:
@@ -67,10 +65,8 @@ def cypher(input, key, key2):
     print("before",str(encryptedTextArray),len(encryptedTextArray))
     encryptedText = ""
     encryptedText = encryptedText.join(encryptedTextArray)
-   # reshaped_text = arabic_reshaper.reshape(encryptedText)
-   # bidi_text = get_display(reshaped_text)
-    #send this to saleh
     return encryptedTextArray
+
 def decypher(text, key, key2):
     encrypted_text_Array=convertInputToArabicPosition(fixTheText(text))
     keyB = key%28
@@ -78,9 +74,7 @@ def decypher(text, key, key2):
     decryptedTextArray= []
     for i in range(len(encrypted_text_Array)):
         if encrypted_text_Array[i] != -1:
-            #the line that handles the encryption of every charecter 1%28
              encrypted_text_Array[i] = ((encrypted_text_Array[i] - (keyB) ) * keysInverse[key2%len(keysInverse)])%(28)
-    print("after",str(encrypted_text_Array),len(encrypted_text_Array))
     for x in encrypted_text_Array:
         if x != -1:
             decryptedTextArray.append(alphapet[x])
@@ -88,10 +82,5 @@ def decypher(text, key, key2):
             decryptedTextArray.append(" ")
     decryptedText = ""
     decryptedText = decryptedText.join(decryptedTextArray)
-    #reshaped_text = arabic_reshaper.reshape(decryptedText)
-    #bidi_text = get_display(reshaped_text)
-    print("affine",decryptedTextArray)
     return decryptedText
 
-
-print(decypher(cypher("السلام عليكم ورحمة",12,12),12,12))
